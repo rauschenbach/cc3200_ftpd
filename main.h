270c57e02e271b252905cb1ae11afd8dc9c1a691
rauschenbach <boxeur@mail.ru>
2018-11-27 13:28:52 +0300

    Add files via upload

---
+++ main.h
@@ -0,0 +1,33 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdarg.h>
+#include <device.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+
+
+#include <simplelink.h>
+#include <hw_common_reg.h>
+#include <hw_memmap.h>
+#include <hw_gprcm.h>
+#include <hw_types.h>
+#include <interrupt.h>
+#include <utils.h>
+#include <prcm.h>
+#include <wlan.h>
+#include <osi.h>
+
+
+#include "userfunc.h"
+#include "globdefs.h"
+#include "sdcard.h"
+#include "board.h"
+#include "led.h"
+#include "ff.h"
+
+
+
+#endif /* main.h */
